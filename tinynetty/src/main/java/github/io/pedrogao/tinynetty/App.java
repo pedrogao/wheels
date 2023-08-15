4ba39fe19dfc7f64ce8d68dedb1f0daca8e12c31
pedrogao <1312342604@qq.com>
2023-08-15 14:34:52 +0800

    feat: init

---
+++ tinynetty/src/main/java/github/io/pedrogao/tinynetty/App.java
@@ -0,0 +1,10 @@
+package github.io.pedrogao.tinynetty;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello TinyNetty!");
+    }
+}
---
+++ tinyrpc/src/main/java/github/io/pedrogao/tinyrpc/App.java
@@ -0,0 +1,10 @@
+package github.io.pedrogao.tinyrpc;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello TinyRPC!");
+    }
+}
